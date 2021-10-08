21e6085e035f667f32c6c9f25c0f68a68fd08f8a
Elena Dermendzhieva <elka.derm@abv.bg>
2021-10-08 15:36:52 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,4 +2,9 @@ const express=require('express')
 
 const app= express();
 
-app.listen(5000, ()=>{ console.log('Application is running on http://5000')})
+app.all('/',(req, res)=>{
+    res.write('It/s work!');
+    res.end()
+})
+
+app.listen(5000, ()=> console.log('Application is running on http://locallhost:5000'))
