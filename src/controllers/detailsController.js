ee347d4f7341d54aeaab1692b05ff61337b0c568
Elena Dermendzhieva <elka.derm@abv.bg>
2021-10-13 12:35:43 +0300

    update details view

--- src/controllers/detailsController.js
+++ src/controllers/detailsController.js
@@ -3,11 +3,14 @@ const router=express.Router();
 const storage= require('../service/storage.js')
 
 async function detailsPage (req, res) {
-     console.log (req.params.id)
+     
     let oneItem= await storage.getOneById(req.params.id)
+
+    // let accesArray= await storage.getAllAccesory(req.params.id)
      
     res.render('details', {
-        one:oneItem
+        one:oneItem,
+        
     })
 }
 async function deleteCub (req,res) {
